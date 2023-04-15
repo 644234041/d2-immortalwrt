191d53d1bbf6a7a5a842827e571d0c39efac1ce1
xgitl <69244012+xgitl@users.noreply.github.com>
2023-04-15 09:34:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,8 +18,7 @@
 # echo 'src-git telephony https://github.com/openwrt/telephony.git' >> feeds.conf.default
 
 
-rm -rf luci/applications/*passwall*
-rm -rf packages/multimedia/*Music*
+find luci/applications -name "*passwall*" -exec rm -rf {} ;
 
 # Add a feed source
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;luci' >>feeds.conf.default
