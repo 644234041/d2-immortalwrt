a4bac6a4ec4e3854168699abe5178cc516949d85
644234041 <123048795+644234041@users.noreply.github.com>
2023-05-18 11:42:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -6,32 +6,39 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Uncomment a feed source
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
-# sed -i '/^src-git luci/d' feeds.conf.default
-# echo 'src-git packages https://github.com/immortalwrt/packages.git' >> feeds.conf.default
-# echo 'src-git routing https://github.com/openwrt/routing.git' >> feeds.conf.default
-# echo 'src-git telephony https://github.com/openwrt/telephony.git' >> feeds.conf.default
-# 下面这条不知道行不行
-# echo 'src/gz openwrt_kiddin9 https://op.supes.top/packages/mipsel_24kc' >> feeds.conf.default
+# 修改主机名
+sed -i 's/ImmortalWrt/Newifi/g' package/base-files/files/bin/config_generate
 
-# rm -rf feeds/luci/applications/luci-app-adbyby-plus
-# rm -rf feeds/luci/themes/luci-theme-argonne
+# Modify the version number
+# sed -i "s/OpenWrt /P3TERX build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
+# Modify default theme
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
+# Modify the version number
+# sed -i "s/OpenWrt /P3TERX build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
-# Add a feed source
-# # # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;luci' >>feeds.conf.default
-# # # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall;packages' >>feeds.conf.default
-# git clone https://github.com/ilxp/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
-# git clone -b master https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
-# svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-adbyby-plus package/luci-app-adbyby-plus
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
-# # echo 'src-git small8 https://github.com/kenzok8/small-package' >> feeds.conf.default
-# # 是最后注释掉的  36行是最后添加的 应该用一个36行就可以了
+# # Add kernel build user
+# [ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
+#     echo 'CONFIG_KERNEL_BUILD_USER="P3TERX"' >>.config ||
+#     sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"P3TERX"@' .config
+
+# # Add kernel build domain
+# [ -z $(grep "CONFIG_KERNEL_BUILD_DOMAIN=" .config) ] &&
+#     echo 'CONFIG_KERNEL_BUILD_DOMAIN="GitHub Actions"' >>.config ||
+#     sed -i 's@\(CONFIG_KERNEL_BUILD_DOMAIN=\).*@\1$"GitHub Actions"@' .config
+
+# Modify default theme
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 删除老argon
+# rm -rf package/lean/luci-theme-argon
+# 拉取argon主题
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
