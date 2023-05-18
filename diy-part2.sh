ae5ff1f535e5608a12770b69996b81ba24f84f99
644234041 <123048795+644234041@users.noreply.github.com>
2023-05-18 11:41:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -6,12 +6,32 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+# Uncomment a feed source
 
-#修改主机名
-sed -i 's/ImmortalWrt/Openwrt/g' package/base-files/files/bin/config_generate
+# sed -i '/^src-git luci/d' feeds.conf.default
+# echo 'src-git packages https://github.com/immortalwrt/packages.git' >> feeds.conf.default
+# echo 'src-git routing https://github.com/openwrt/routing.git' >> feeds.conf.default
+# echo 'src-git telephony https://github.com/openwrt/telephony.git' >> feeds.conf.default
+# 下面这条不知道行不行
+# echo 'src/gz openwrt_kiddin9 https://op.supes.top/packages/mipsel_24kc' >> feeds.conf.default
+
+# rm -rf feeds/luci/applications/luci-app-adbyby-plus
+# rm -rf feeds/luci/themes/luci-theme-argonne
+
+
+
+# Add a feed source
+# # # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;luci' >>feeds.conf.default
+# # # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall;packages' >>feeds.conf.default
+# git clone https://github.com/ilxp/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
+# git clone -b master https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
+# svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-adbyby-plus package/luci-app-adbyby-plus
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+# # echo 'src-git small8 https://github.com/kenzok8/small-package' >> feeds.conf.default
+# # 是最后注释掉的  36行是最后添加的 应该用一个36行就可以了
